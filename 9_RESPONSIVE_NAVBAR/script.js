4b560a1430e2bb82539318a3031d71c6fcc25cb0
Kanish <111358462+kanish13@users.noreply.github.com>
2023-08-12 20:07:16 +0530

    Delete 9_RESPONSIVE_NAVBAR directory

--- 9_RESPONSIVE_NAVBAR/script.js
+++
@@ -1,7 +0,0 @@
-const butt = document.querySelector("button");
-const element = document.querySelector(".menu-after");
-
-butt.addEventListener("click", () => {
-    element.classList.toggle("hide");
-});
-
