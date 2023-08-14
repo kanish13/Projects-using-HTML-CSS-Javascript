9e2a07636f09b3353a647213c5d84369604e32d6
Kanish <111358462+kanish13@users.noreply.github.com>
2023-08-14 14:53:39 +0530

    Add files via upload

---
+++ 10_MODAL_PROJECT/script.js
@@ -0,0 +1,14 @@
+let butt=document.querySelector(".clickme");
+let over=document.querySelector(".over");
+let butt2=document.querySelector(".close")
+butt.addEventListener("click",()=>{
+ 
+    over.style.display="flex";
+  
+})
+
+butt2.addEventListener("click",()=>{
+ 
+    over.style.display="none";
+  
+})
